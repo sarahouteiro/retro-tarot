d904ae8b6b73d74633f1ea5da8874d0b7b2a87ee
Sarah Outeiro <sarahdinizouteiro@mgmail.com>
2022-10-06 10:44:32 -0300

    ajustes responsivos

--- src/pages/CardDetail/styled.js
+++ src/pages/CardDetail/styled.js
@@ -7,6 +7,11 @@ export const BotaoDiv = styled.div`
     justify-content: space-between;
     margin: 0 auto;
     padding: 70px 0 20px 0;
+    @media (max-width: 500px) {
+        flex-direction: column;
+        max-width: 300px;
+        row-gap: 10px;
+    }
 `
 
 export const Container = styled.div`
@@ -17,11 +22,21 @@ export const Container = styled.div`
     background-color: #DC98BD;
     border-radius: 50px;
     border: 3px groove #E54F24;
+    @media (max-width: 500px) {
+        max-width: 350px;
+        flex-direction: column;
+    }
 `
 
 export const ImageDiv = styled.div`
     margin: 100px 0 0 100px;
     padding-bottom: 100px;
+    @media (max-width: 500px) {
+        text-align: center;
+        margin: 0 auto;
+        padding-top: 50px;
+        padding-bottom: 20px;
+    }
 `
 
 export const TextDiv = styled.div`
@@ -30,4 +45,9 @@ export const TextDiv = styled.div`
     justify-content: center;
     margin: 120px 0 0 80px;
     padding-bottom: 100px;
+    @media (max-width: 500px) {
+        max-width: 300px;
+        margin: 0 auto;
+        padding-bottom: 50px;
+    }
 `
--- src/pages/ErrorPage/styled.js
+++ src/pages/ErrorPage/styled.js
@@ -6,10 +6,16 @@ export const Container = styled.div`
     display: flex;
     flex-direction: column;
     max-width:500px;
+    @media (max-width: 500px) {
+        max-width: 300px;
+    }
 `
 
 export const Error = styled.img`
     margin-top: 200px;
     width: 500px;
     margin-bottom: 50px;
+    @media (max-width: 500px) {
+        max-width: 300px;
+    }
 `
